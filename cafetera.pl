af7d82f815970f7ebbebe9675e002622c79e370a
Franco <franco.leal@usach.cl>
2019-05-06 16:56:13 -0400

    prepararCafe

--- cafetera.pl
+++ cafetera.pl
@@ -6,13 +6,13 @@ preparacion(americano,[7,0,0,60]).
 preparacion(cortado,[7,3,0,50]).
 preparacion(capuccino,[7,19,0,150]).
 preparacion(latte,[7,9,0,90]).
-preparacion(latte,[7,9,3,100]).
+preparacion(mokaccino,[7,9,3,100]).
 
-% Tamaños de taza: el numero indica el multiplicador de la cantidad de
+% Tamaï¿½os de taza: el numero indica el multiplicador de la cantidad de
 % cada ingrediente
 taza(grande,2).
 taza(mediana,1).
-taza(pequeña,1/2).
+taza(pequeï¿½a,1/2).
 
 %Intensidad del cafe segun el tipo
 intensidad(arabica,suave).
@@ -20,7 +20,7 @@ intensidad(robusta,fuerte).
 intensidad(combinado,medio).
 intensidad(descafeinado,descafeinado).
 
-%Tiempo de preparacion segun estacion del año
+%Tiempo de preparacion segun estacion del aï¿½o
 tiempo(verano,60).
 tiempo(primavera,90).
 tiempo(invierno,120).
@@ -28,7 +28,7 @@ tiempo(invierno,120).
 multiplicarLista(Numero,[H],[Salida]):- Salida is Numero*H.
 multiplicarLista(Numero,[H|T],Salida):- multiplicarLista(Numero,T,X),Resultado is Numero*H, append([Resultado],X,Salida).
 
-% prepararCafe(TamañoTaza, TipoPreparacion, TipoCafe, EstacionAño,
-% Salida).
+prepararCafe(TamaÃ±o, TipoP, TipoC, Estacion, Salida):- taza(TamaÃ±o,T), preparacion(TipoP,[Comp1,Comp2,Comp3,Comp4]), intensidad(TipoC,Intensidad),tiempo(Estacion,Tiempo), S1 is Comp1*T, S2 is Comp2*T, S4 is Comp4*T, append([[S1],[S2],[S4],[Intensidad],[Tiempo]], Salida).
+
 
 sePuedeUsar(Instalada,CantidadAgua,CantidadCafe,CantidadLeche):- Instalada = si, CantidadAgua >= 150, CantidadCafe >= 30, CantidadLeche >= 30.
