5c0e354d6ed36b946559bee4725d566919934ba3
Nicolas <nicolas.romero.f@usach.cl>
2019-05-06 19:37:17 -0400

    intensidad de cafe

--- cafetera.pl
+++ cafetera.pl
@@ -36,40 +36,51 @@ cantAgua(mokaccino,100).
 % cada ingrediente
 taza(grande,2).
 taza(mediana,1).
-taza(pequeña,0.5).
+taza(pequena,0.5).
 
 %Intensidad del cafe segun el tipo
 intensidad(arabica,suave).
 intensidad(robusta,fuerte).
 intensidad(combinado,medio).
-intensidad(descafeinado,descafeinado).
+intensidad(descafeinado,suave).
+
+tipoIntensidad(1,suave).
+tipoIntensidad(2,medio).
+tipoIntensidad(3,fuerte).
 
 %Tiempo de preparacion segun estacion del a�o
 tiempo(verano,60).
 tiempo(primavera,90).
 tiempo(invierno,120).
+tiempo(otono,90).
 
 multiplicarLista(Numero,[H],[Salida]):- Salida is Numero*H.
 multiplicarLista(Numero,[H|T],Salida):- multiplicarLista(Numero,T,X),Resultado is Numero*H, append([Resultado],X,Salida).
 
-prepararCafe(Tamaño, TipoP, TipoC, Estacion, Salida):- taza(Tamaño,T), preparacion(TipoP,[Comp1,Comp2,Comp3,Comp4]), intensidad(TipoC,Intensidad),tiempo(Estacion,Tiempo), S1 is Comp1*T, S2 is Comp2*T, S4 is Comp4*T, append([[S1],[S2],[S4],[Intensidad],[Tiempo]], Salida).
+prepararCafe(Tamano, TipoP, TipoC, Estacion, Salida):- taza(Tamano,T), preparacion(TipoP,[Comp1,Comp2,_,Comp4]), intensidad(TipoC,Intensidad),tiempo(Estacion,Tiempo), S1 is Comp1*T, S2 is Comp2*T, S4 is Comp4*T, append([[S1],[S2],[S4],[Intensidad],[Tiempo]], Salida).
 
 division(Numero1, Numero2, Salida):- Salida is Numero1/Numero2.
 
-buscarMenor(Tamaño, espresso, CantidadCafe,CantidadLeche, CantidadAgua,Salida):- cantCafe(CantCafe), division(CantidadCafe,CantCafe,X1), cantAgua(espresso,CantAgua),division(CantidadAgua,CantAgua,X2), append([[X1],[X2]],SalidaAux), min_list(SalidaAux,Salida).
+buscarMenor(espresso, CantidadCafe, _, CantidadAgua,Salida):- cantCafe(CantCafe), division(CantidadCafe,CantCafe,X1), cantAgua(espresso,CantAgua),division(CantidadAgua,CantAgua,X2), append([[X1],[X2]],SalidaAux), min_list(SalidaAux,Salida).
 
-buscarMenor(Tamaño, americano, CantidadCafe,CantidadLeche, CantidadAgua,Salida):- cantCafe(CantCafe), division(CantidadCafe,CantCafe,X1), cantAgua(americano,CantAgua),division(CantidadAgua,CantAgua,X2), append([[X1],[X2]],SalidaAux), min_list(SalidaAux,Salida).
+buscarMenor(americano, CantidadCafe, _, CantidadAgua,Salida):- cantCafe(CantCafe), division(CantidadCafe,CantCafe,X1), cantAgua(americano,CantAgua),division(CantidadAgua,CantAgua,X2), append([[X1],[X2]],SalidaAux), min_list(SalidaAux,Salida).
 
-buscarMenor(Tamaño, cortado, CantidadCafe,CantidadLeche, CantidadAgua,Salida):- cantCafe(CantCafe), division(CantidadCafe,CantCafe,X1), cantLeche(cortado,CantLeche), division(CantidadLeche,CantLeche,X2), cantAgua(cortado,CantAgua),division(CantidadAgua,CantAgua,X3), append([[X1],[X2],[X3]],SalidaAux), min_list(SalidaAux,Salida).
+buscarMenor(cortado, CantidadCafe,CantidadLeche, CantidadAgua,Salida):- cantCafe(CantCafe), division(CantidadCafe,CantCafe,X1), cantLeche(cortado,CantLeche), division(CantidadLeche,CantLeche,X2), cantAgua(cortado,CantAgua),division(CantidadAgua,CantAgua,X3), append([[X1],[X2],[X3]],SalidaAux), min_list(SalidaAux,Salida).
 
-buscarMenor(Tamaño, cappuccino, CantidadCafe,CantidadLeche, CantidadAgua,Salida):- cantCafe(CantCafe), division(CantidadCafe,CantCafe,X1), cantLeche(cappuccino,CantLeche), division(CantidadLeche,CantLeche,X2), cantAgua(cappuccino,CantAgua),division(CantidadAgua,CantAgua,X3), append([[X1],[X2],[X3]],SalidaAux), min_list(SalidaAux,Salida).
+buscarMenor(cappuccino, CantidadCafe,CantidadLeche, CantidadAgua,Salida):- cantCafe(CantCafe), division(CantidadCafe,CantCafe,X1), cantLeche(cappuccino,CantLeche), division(CantidadLeche,CantLeche,X2), cantAgua(cappuccino,CantAgua),division(CantidadAgua,CantAgua,X3), append([[X1],[X2],[X3]],SalidaAux), min_list(SalidaAux,Salida).
 
-buscarMenor(Tamaño, latte, CantidadCafe,CantidadLeche, CantidadAgua,Salida):- cantCafe(CantCafe), division(CantidadCafe,CantCafe,X1), cantLeche(latte,CantLeche), division(CantidadLeche,CantLeche,X2), cantAgua(latte,CantAgua),division(CantidadAgua,CantAgua,X3), append([[X1],[X2],[X3]],SalidaAux), min_list(SalidaAux,Salida).
+buscarMenor(latte, CantidadCafe,CantidadLeche, CantidadAgua,Salida):- cantCafe(CantCafe), division(CantidadCafe,CantCafe,X1), cantLeche(latte,CantLeche), division(CantidadLeche,CantLeche,X2), cantAgua(latte,CantAgua),division(CantidadAgua,CantAgua,X3), append([[X1],[X2],[X3]],SalidaAux), min_list(SalidaAux,Salida).
 
-buscarMenor(Tamaño, mokaccino, CantidadCafe,CantidadLeche, CantidadAgua,Salida):- cantCafe(CantCafe), division(CantidadCafe,CantCafe,X1), cantLeche(mokaccino,CantLeche), division(CantidadLeche,CantLeche,X2), cantAgua(mokaccino,CantAgua),division(CantidadAgua,CantAgua,X3),append([[X1],[X2],[X3]],SalidaAux), min_list(SalidaAux,Salida).
+buscarMenor(mokaccino, CantidadCafe,CantidadLeche, CantidadAgua,Salida):- cantCafe(CantCafe), division(CantidadCafe,CantCafe,X1), cantLeche(mokaccino,CantLeche), division(CantidadLeche,CantLeche,X2), cantAgua(mokaccino,CantAgua),division(CantidadAgua,CantAgua,X3),append([[X1],[X2],[X3]],SalidaAux), min_list(SalidaAux,Salida).
 
-cantidadTazas(TamañoTaza, TipoPreparacion, TipoCafe, EstacionAño, CantidadCafe, CantidadLeche, CantidadAgua, Salida):- taza(TamañoTaza, Tamaño), buscarMenor(Tamaño, TipoPreparacion, CantidadCafe/Tamaño, CantidadLeche/Tamaño, CantidadAgua/Tamaño, SalidaAux), X1 is floor(SalidaAux), tiempo(EstacionAño, Tiempo),X2 is X1*Tiempo, append([[X1],[X2]], Salida).
+cantidadTazas(TamanoTaza, TipoPreparacion, _, EstacionAno, CantidadCafe, CantidadLeche, CantidadAgua, Salida):- taza(TamanoTaza, Tamano), buscarMenor(TipoPreparacion, CantidadCafe/Tamano, CantidadLeche/Tamano, CantidadAgua/Tamano, SalidaAux), X1 is floor(SalidaAux), tiempo(EstacionAno, Tiempo),X2 is X1*Tiempo, append([[X1],[X2]], Salida).
 
 sePuedeUsar(Instalada,CantidadAgua,CantidadCafe,CantidadLeche):- Instalada = si, CantidadAgua >= 150, CantidadCafe >= 30, CantidadLeche >= 30.
 
-intensidadCafe(TipoCafe, TipoPreparacion, Salida):- intensidad(TipoCafe,Salida).
+comparacionCL(CF,CL,Salida):- CF >= CL, Salida is 0.
+comparacionCL(CF,CL,Salida):- CF < CL, Salida is 1. 
+
+intensidadPreparacion(IntensidadCafe,TipoPreparacion,Salida):- cantCafe(CF), cantLeche(TipoPreparacion,CL), comparacionCL(CF,CL,X),Res is IntensidadCafe-X, tipoIntensidad(Res,Salida).
+
+
+intensidadCafe(TipoCafe, TipoPreparacion, Salida):- intensidad(TipoCafe,Intensidad), tipoIntensidad(TipoInt,Intensidad), intensidadPreparacion(TipoInt,TipoPreparacion,Salida).
